654bc9699c8dad63b2ad0103b22b56c2b57073c0
Jeremy Gillmore <happy12287@gmail.com>
2017-02-18 12:46:12 -0600

    template finished (i think)

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,1 +1,5 @@
-console.log('loading mongoose connection and model');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/{{template}}');
+
+require('../models/{{template}}');
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,1 +1,6 @@
-console.log("loading routes");
+var {{template}}Controller = require('../controllers/{{template}}');
+module.exports = function(app){
+  app.get('/{{template}}', function(req,res){
+    {{template}}Controller.showAll(req,res);
+  });
+}
